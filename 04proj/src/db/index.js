b53a3954fea3dc0bd9deab55aff517a6aefe78a1
harmeetsran01 <harmeetsran01@gmail.com>
2026-04-07 10:13:45 +0530

    Connection Successful with DNS

--- 04proj/src/db/index.js
+++ 04proj/src/db/index.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
+import dns from "dns"
+dns.setServers([
+    '1.1.1.1'
+])
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`Connection Initialized: ${process.env.MONGO_URI}`)
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, { dbName: DB_NAME })
         console.log(`Connected to MongoDB !! DB HOST: ${connectionInstance.connection.host}`)
     }
     catch (e) {
