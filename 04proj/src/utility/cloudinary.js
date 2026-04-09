1256b54d341d0f0b9dd0e3eb3236df7bac72d8d7
harmeetsran01 <harmeetsran01@gmail.com>
2026-04-09 14:08:12 +0530

    Intiailize Cloudinary

---
+++ 04proj/src/utility/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs" //File system, avail default in node, File handling
+// Unlink in fs meanss to remove file
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })``
+        console.log("file is uploaded on cloudinary", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved file as failed to upload
+        console.log(error)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
