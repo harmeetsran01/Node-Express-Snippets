0c1cea35f15f3ae98dc4cc80d6153a1c943b7ac9
harmeetsran01 <harmeetsran01@gmail.com>
2026-04-29 00:59:27 +0530

    Some DNS changes for mongo

--- 04proj/src/controllers/user.controller.js
+++ 04proj/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utility/asynchandler.js";
-import { ApiError } from "../utility/ApiError.js";
-import {User} from "../models/user.models.js"
-import {uploadOnCloudinary } from "../utility/cloudinary.js";
-import { ApiResponse } from "../utility/ApiResponse.js";
+import ApiError from "../utility/apiError.js";
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utility/cloudinary.js";
+import ApiResponse from "../utility/apiResponse.js";
 
 
 /** 
@@ -21,67 +21,67 @@ import { ApiResponse } from "../utility/ApiResponse.js";
  * req.body: request from body, json or form {fullname,email,username,password}
  * req.files: request from files []
  */
-const registerUser = asyncHandler(async (req,res)=>{
+const registerUser = asyncHandler(async (req, res) => {
 
-  const {email} = req.body
+  const { fullname, email, username, password } = req.body
   console.log(email)
-  
+
   // return res.status(200).json({
   //   message:"User registered successfully"
   // })
 
-  if(email ===""){
-    throw new ApiError(400,"Email is required")
+  if (email === "") {
+    throw new ApiError(400, "Email is required")
   }
   // to check more errors at one go
-  if(
-    [fullname,email,username,password].some((field)=>field?.trim() === "")
-  ) throw new ApiError(400,"All fields are required")
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) throw new ApiError(400, "All fields are required")
 
-  const existedUser = User.findOne({
-    $or:[{email},{username}]
+  const existedUser = await User.findOne({
+    $or: [{ email }, { username }]
   })
-  
-  if(existedUser){
-    throw new ApiError(400,"User already exists")
+
+  if (existedUser) {
+    throw new ApiError(400, "User already exists")
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path
   const coverLocalPath = req.files?.coverImage[0]?.path
 
-  if(!avatarLocalPath)
-    throw new ApiError(400,"avatar is required")
+  if (!avatarLocalPath)
+    throw new ApiError(400, "avatar is required")
 
   const avatarUrl = await uploadOnCloudinary(avatarLocalPath)
   const coverImageUrl = await uploadOnCloudinary(coverLocalPath)
-  
+
 
   //check url exists or not if not throw error
-  if(!avatarUrl || !coverImageUrl)
-    throw new ApiError(400,"Something went wrong while uploading")
-    
-const user = await User.create({
-  fullname,
-  email:email.toLowerCase(),
-  username:username.toLowerCase(),
-  password,
-  avatar: avatarUrl.url,
-  coverImage: coverImageUrl?.url || "",
+  if (!avatarUrl || !coverImageUrl)
+    throw new ApiError(400, "Something went wrong while uploading")
+
+  const user = await User.create({
+    fullname,
+    email: email.toLowerCase(),
+    username: username.toLowerCase(),
+    password,
+    avatar: avatarUrl.url,
+    coverImage: coverImageUrl?.url || "",
+  })
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"   //use(- minus)to exclude password
+  )
+
+  if (!createdUser)
+    throw new ApiError(500, "Something went wrong while creating user")
+
+  return res.status(200).json(new ApiResponse(
+    200,
+    createdUser,
+    "User registered successfully",
+  ))
+
 })
 
-const createdUser = await User.findById(user._id).select(
-  "-password -refreshToken"   //use(- minus)to exclude password
-)
-
-if(!createdUser)
-  throw new ApiError(500,"Something went wrong while creating user")
-
-return res.status(200).json(new ApiResponse(
-  200,
-  createdUser,
-  "User registered successfully",
-))
-
-})
-
-export {registerUser}
+export { registerUser }
--- 04proj/src/db/index.js
+++ 04proj/src/db/index.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 import dns from "dns"
 dns.setServers([
-    '1.1.1.1'
+    '1.1.1.1',
+    '8.8.8.8'
 ])
 
 const connectDB = async () => {
     try {
         console.log(`Connection Initialized: ${process.env.MONGO_URI}`)
-        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, { dbName: DB_NAME })
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, { dbName: DB_NAME, serverSelectionTimeoutMS: 15000 })
         console.log(`Connected to MongoDB !! DB HOST: ${connectionInstance.connection.host}`)
     }
     catch (e) {
