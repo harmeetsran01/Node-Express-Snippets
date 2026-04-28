47fe3c8f01e6159dd8557b0e8b875d9cf05581fc
harmeetsran01 <harmeetsran01@gmail.com>
2026-04-29 00:00:07 +0530

    User Module done

--- 04proj/src/controllers/user.controller.js
+++ 04proj/src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import { asyncHandler } from "../utility/asynchandler.js";
 import { ApiError } from "../utility/ApiError.js";
 import {User} from "../models/user.models.js"
+import {uploadOnCloudinary } from "../utility/cloudinary.js";
+import { ApiResponse } from "../utility/ApiResponse.js";
+
 
 /** 
  * Validation
@@ -42,6 +45,43 @@ const registerUser = asyncHandler(async (req,res)=>{
   if(existedUser){
     throw new ApiError(400,"User already exists")
   }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path
+  const coverLocalPath = req.files?.coverImage[0]?.path
+
+  if(!avatarLocalPath)
+    throw new ApiError(400,"avatar is required")
+
+  const avatarUrl = await uploadOnCloudinary(avatarLocalPath)
+  const coverImageUrl = await uploadOnCloudinary(coverLocalPath)
+  
+
+  //check url exists or not if not throw error
+  if(!avatarUrl || !coverImageUrl)
+    throw new ApiError(400,"Something went wrong while uploading")
+    
+const user = await User.create({
+  fullname,
+  email:email.toLowerCase(),
+  username:username.toLowerCase(),
+  password,
+  avatar: avatarUrl.url,
+  coverImage: coverImageUrl?.url || "",
+})
+
+const createdUser = await User.findById(user._id).select(
+  "-password -refreshToken"   //use(- minus)to exclude password
+)
+
+if(!createdUser)
+  throw new ApiError(500,"Something went wrong while creating user")
+
+return res.status(200).json(new ApiResponse(
+  200,
+  createdUser,
+  "User registered successfully",
+))
+
 })
 
 export {registerUser}
