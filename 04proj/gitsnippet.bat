32cb1d6b195ecd985b30ad359a8f01973c80c502
harmeetsran01 <harmeetsran01@gmail.com>
2026-04-30 14:36:17 +0530

    file added commit

---
+++ 04proj/gitsnippet.bat
@@ -0,0 +1,15 @@
+@echo off
+set /p msg="Enter commit message: "
+
+cd "E:\Project\Backend"
+
+:: 1. Add the specific folder
+git add "04proj"
+
+:: 2. Commit with the input message
+git commit -m "%msg%"
+
+:: 3. Push to origin master
+git push origin master
+
+pause
